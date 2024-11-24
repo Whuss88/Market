89c6f4e553d68ff4ed6d49af20b9e5b10436c80f
Whuss88 <williamhussey95@gmail.com>
2024-11-24 13:28:46 -0500

    orders router has been authenticated and has post and gets

--- routes/orders.js
+++ routes/orders.js
@@ -1,3 +1,47 @@
 const express = require("express");
 const prisma = require("../prisma");
 const authenicate = require("../middleware/authenticate");
+const router = express.Router();
+
+router.use(authenicate);
+
+router.get("/", async (req, res, next) => {
+  try {
+    const order = await prisma.order.findMany({ where: {userId: req.user.id }, incldude: { products: true} });
+    res.json(orders);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  const { date, note, productIds } = req.body;
+  try {
+    const order = await prisma.order.create({
+      data: {
+        createdAt: new Date(date),
+        note,
+        userId: req.user.id,
+        products: { connect: productIds.map(id => ({ id})) }
+      }
+    });
+    res.status(201).json(order);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const order = await prisma.order.findUnique({ where: { id: parseInt(id)} });
+    if (!order || order.userId !== req.user.id) {
+      return res.status(403).json({ error: `Forbidden`});
+    }
+    res.json(order);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
